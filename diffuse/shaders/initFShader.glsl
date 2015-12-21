f2deaaf8655b1ed044fb987fadda1b163cdcfaef
JGM <jdgalaz@uc.cl>
2015-12-21 20:30:45 -0300

    (diffuse) inverted colors in initial condition

--- diffuse/shaders/initFShader.glsl
+++ diffuse/shaders/initFShader.glsl
@@ -7,5 +7,5 @@ void main()
 
     float value = texture2D(tSource, vUv).r;
 
-    gl_FragColor = vec4(value*colors[6].a, 0.0, 0.0, 1.0); 
+    gl_FragColor = vec4((1.0-value)*colors[6].a, 0.0, 0.0, 1.0); 
 }
